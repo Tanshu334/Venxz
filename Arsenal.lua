70cd8ee51a92b70291e36f6a8bf76ddedb6a2f3a
Tanshu334 <94998344+Tanshu334@users.noreply.github.com>
2022-11-08 01:30:42 +0530

    Update Arsenal.lua

--- Arsenal.lua
+++ Arsenal.lua
@@ -24,6 +24,24 @@ btns:Button(
 
 )
 
+btns:Dropdown(
+
+    "Choose AimPart!",
+
+    {"Head", "Body", "Leg"},
+
+    function(bool)
+
+if table[1] == bool then
+
+function getplrsname() for i,v in pairs(game:GetChildren()) do if v.ClassName == "Players" then return v.Name end end end local players = getplrsname() local plr = game[players].LocalPlayer coroutine.resume(coroutine.create(function() while wait(1) do coroutine.resume(coroutine.create(function() for _,v in pairs(game[players]:GetPlayers()) do if v.Name ~= plr.Name and v.Character then v.Character.RightUpperLeg.CanCollide = false v.Character.RightUpperLeg.Transparency = 10 v.Character.RightUpperLeg.Size = Vector3.new(0,0,0) v.Character.LeftUpperLeg.CanCollide = false v.Character.LeftUpperLeg.Transparency = 10 v.Character.LeftUpperLeg.Size = Vector3.new(0,0,0) v.Character.HeadHB.CanCollide = false v.Character.HeadHB.Transparency = 10 v.Character.HeadHB.Size = Vector3.new(21,21,21) v.Character.HumanoidRootPart.CanCollide = false v.Character.HumanoidRootPart.Transparency = 10 v.Character.HumanoidRootPart.Size = Vector3.new(0,0,0) end end end)) end end))
+
+      end
+
+    end
+
+)
+
 local ESP = serv:Channel("ESP")
 
 ESP:Toggle(
